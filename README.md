54b1f5fd8f3c6ad4c0488f33bf658bfacb2024ff
michaeldomanek <michaelDo.spam@gmail.com>
2021-02-14 11:41:14 +0100

    add smfl and example

--- README.md
+++ README.md
@@ -5,15 +5,15 @@
 Netzwerk‐basiertes RobotGame wie robowiki mit zentralem Server.
 
 ## verwendete Software
-- [asio](https://think-async.com/Asio/)
-- [CLI11](https://github.com/CLIUtils/CLI11)
-- [criterion](https://github.com/p-ranav/criterion)
-- [fmt](https://github.com/fmtlib/fmt)
-- [spdlog](https://github.com/gabime/spdlog)
+* [asio](https://think-async.com/Asio/)
+* [CLI11](https://github.com/CLIUtils/CLI11)
+* [criterion](https://github.com/p-ranav/criterion)
+* [fmt](https://github.com/fmtlib/fmt)
+* [spdlog](https://github.com/gabime/spdlog)
 
 
-- [protocol-buffers](https://developers.google.com/protocol-buffers/)
-- [gRPC](http://www.grpc.io/)
+* [protocol-buffers](https://developers.google.com/protocol-buffers/)
+* [gRPC](http://www.grpc.io/)
 
 
-- [sfml](https://www.sfml-dev.org/index.php)
+* [sfml](https://github.com/SFML/SFML)
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,41 @@
-
+#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <thread>
+#include <cstdint>
+#include <unistd.h>
 
 using namespace std;
 
-//int main(int argc, char* argv[]) {
-int main() {
-    thread t{[]{ cout << "Hello"; }};
-    t.join();
-    cout << " world!" << endl;
+int main()
+{
+    const unsigned width = 700;
+    const unsigned height= 700;
+
+    sf::RenderWindow window(sf::VideoMode(width, height), "Robotgame");
+
+    sf::Texture texture;
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setTextureRect(sf::IntRect(0, 0, 70, 70));
+    sprite.setColor(sf::Color(255, 255, 255, 200));
+    sprite.setPosition(100, 25);
+
+    while (window.isOpen()) {
+
+        window.clear(sf::Color::Black);
+
+        sprite.move(1, 1);
+
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.draw(sprite);
+        window.display();
+        
+        usleep(10000);
+    }
+
     return 0;
 }

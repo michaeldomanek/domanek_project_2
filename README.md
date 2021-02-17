66a94abacab6578fd0c6c6ae32d0bf00b934d8fc
michaeldomanek <michaelDo.spam@gmail.com>
2021-02-17 12:21:48 +0100

    project clean up

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ Netzwerk‐basiertes RobotGame wie robowiki mit zentralem Server.
 ## Spiel
 
 Das Spiel ist eine abgeänderte Version von [robowiki](https://robowiki.net/wiki/Main_Page).
-Die Resourcen des Spiel wurden von [robocode](https://github.com/robo-code/robocode) verwendet.
+
+Die Ressourcen für das Spiel wurden von [robocode](https://github.com/robo-code/robocode) verwendet.
 
 ## verwendete Software
 * [asio](https://think-async.com/Asio/)
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,12 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
-#include <algorithm>
 
 using namespace std;
 
 const unsigned int width = 700;
 const unsigned int height = 700;
-const unsigned int robotWidth = 36;
+const unsigned int robotWidth = 38;
 
 int main() {
 
@@ -20,7 +19,7 @@ int main() {
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
     sf::Texture robotTexture;
-    robotTexture.loadFromFile("../src/resources/body.png");
+    robotTexture.loadFromFile("../src/resources/body2.png");
 
     sf::Texture turretTexture;
     turretTexture.loadFromFile("../src/resources/turret.png");
@@ -36,26 +35,9 @@ int main() {
     robot.setPosition(200, 200);
     turret.setPosition(200, 200);
 
-    // vector<sf::FloatRect> walls;
-    // walls.push_back(sf::FloatRect(0, 0, 700, 1));
-    // walls.push_back(sf::FloatRect(0, 0, 1, 700));
-    // walls.push_back(sf::FloatRect(700, 700, -700, 1));
-    // walls.push_back(sf::FloatRect(700, 700, 1, -700));
-
-    // bool intersects = false;
 
     while (window.isOpen()) {
         window.clear(sf::Color::White);
-
-        // sf::FloatRect player = robot.getGlobalBounds();
-        
-        // intersects = false;
-
-        // for (auto const& wall: walls) {
-        //     intersects = intersects || player.intersects(wall);
-        // }
-
-        // if (!intersects) {
 
         if (sf::Keyboard::isKeyPressed( sf::Keyboard::A )) {
             robot.setRotation(robot.getRotation() - 1);
@@ -63,11 +45,11 @@ int main() {
             robot.setRotation(robot.getRotation() + 1);
         }
 
-        if (sf::Keyboard::isKeyPressed( sf::Keyboard::W )) {
+        if (sf::Keyboard::isKeyPressed( sf::Keyboard::S )) {
             float angle = robot.getRotation() * M_PI / 180;
             robot.move(sin(angle) * -2, cos(angle) * 2);
             turret.move(sin(angle) * -2, cos(angle) * 2);
-        } else if (sf::Keyboard::isKeyPressed( sf::Keyboard::S )) {
+        } else if (sf::Keyboard::isKeyPressed( sf::Keyboard::W )) {
             float angle = robot.getRotation() * M_PI / 180;
             robot.move(sin(angle) * 2, cos(angle) * -2);
             turret.move(sin(angle) * 2, cos(angle) * -2);
@@ -78,24 +60,6 @@ int main() {
         } else if (sf::Keyboard::isKeyPressed( sf::Keyboard::E )) {
             turret.setRotation(turret.getRotation() + 1.5);
         }
-
-        // } else {
-        //     cout << "intersect" << endl;
-        // }
-        
-        
-
-        // if (sprite.getRotation() == 0 && sprite.getPosition().y >= height - sprite.getGlobalBounds().height / 2) {
-        //     sprite.setRotation(90);
-        //     cout << "1. border" << endl;
-        //     moveX = 1;
-        //     moveY = 0;
-        // } else if (sprite.getRotation() == 90 && sprite.getPosition().x >= width - sprite.getGlobalBounds().width / 2) {
-        //     sprite.setRotation(180);
-        //     cout << "2. border" << endl;
-        //     moveX = 0;
-        //     moveY = -1;
-        // }
 
         sf::Event event;
         while (window.pollEvent(event)) {
